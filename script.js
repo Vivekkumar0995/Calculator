705928682bd6bba8149cb74863604b66927607ad
vivek kumar <vivekkumarx405@gmail.com>
2025-02-18 15:08:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function clearDisplay(){
+    document.getElementById('display').value = '';
+}
+function appendValue(value){
+    document.getElementById('display').value += value; 
+}
+function calculateResult(){
+    try{
+    document.getElementById('display').value = eval(document.getElementById('display').value);
+    }
+    catch{
+        document.getElementById('display').value = 'error';
+    }
+}
